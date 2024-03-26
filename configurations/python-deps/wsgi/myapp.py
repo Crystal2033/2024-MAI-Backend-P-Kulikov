472a0d0848d1843ce5151c41b7cf73c967c72499
Kulikov Pavel <kulikov-pavlik@bk.ru>
2024-03-26 19:29:01 +0300

    Added essential names and chaged plain/text in gunicorn on image/png

--- configurations/python-deps/wsgi/myapp.py
+++ configurations/python-deps/wsgi/myapp.py
@@ -1,10 +1,9 @@
-# with open('fileWithHtml.txt', 'r', encoding='utf-8') as f:
-#     file_contents = f.read()  # Чтение содержимого файла в строку
-# data = f"{file_contents}".encode('utf-8')
 def app(environ, start_response):
-    data = b"Hello world!\n"
+    with open('H-P-Lovecraft.png', 'rb') as f:
+        file_contents = f.read()
+    data = file_contents
     start_response("200 OK", [
-        ("Content-Type", "text/plain"),
+        ("Content-Type", "image/png"),
         ("Content-Length", str(len(data)))
     ])
     return iter([data])
--- configurations/python-deps/wsgi/run_app.sh
+++ configurations/python-deps/wsgi/run_app.sh
@@ -8,11 +8,13 @@ readonly WORKERS_CNT=4
 
 function main()
 {
-  apt-get -y update
-  apt-get install -y iproute2
-  apt-get -y install wrk
-  ip a
-  if ! gunicorn -b 0.0.0.0:81 myapp:app ; then
+  #-----------THIS IS FOR WRK ANALYSIS
+  #apt-get -y update
+  #apt-get install -y iproute2
+  #apt-get -y install wrk
+  #ip a
+  #-----------THIS IS FOR WRK ANALYSIS-----------
+  if ! gunicorn --workers ${WORKERS_CNT} -b 0.0.0.0:81 myapp:app ; then
     echo "Failed to run gunicorn..."
     return 1
   fi
