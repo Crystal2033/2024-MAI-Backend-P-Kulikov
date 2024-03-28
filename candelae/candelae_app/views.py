6d8bc6bf59da13015849b475b5f486f9cd179d75
Kulikov Pavel <kulikov-pavlik@bk.ru>
2024-03-28 15:35:17 +0300

    Added GET req for returning jsonResponse. Note: start project with port 9999:  "python manage.py runserver 9999"

--- candelae/candelae_app/views.py
+++ candelae/candelae_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def check_json(request):
+    return JsonResponse({"name": "Paul", "age": 22})
 
 
 def home(request):
